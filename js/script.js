648601ac3f689539dcbb68280d9bc7d866368f07
Hakan Shabran <hakanshabran828@gmail.com>
2026-09-02 21:39:05 +0700

    Initial website

---
+++ js/script.js
@@ -0,0 +1,12 @@
+﻿// Mobile navigation toggle
+(function () {
+    var toggle = document.querySelector(".nav-toggle");
+    var mobileNav = document.querySelector(".nav-mobile");
+
+    if (!toggle || !mobileNav) return;
+
+    toggle.addEventListener("click", function () {
+        var isOpen = mobileNav.classList.toggle("open");
+        toggle.setAttribute("aria-expanded", isOpen ? "true" : "false");
+    });
+})();
